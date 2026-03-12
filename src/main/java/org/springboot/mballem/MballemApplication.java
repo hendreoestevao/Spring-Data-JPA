b5dc43e396c5e3265219e5128561eebfff8d3894
hendreo santos <hendreosantos1@outlook.com>
2026-03-11 21:00:32 -0300

    iniciando projeto

---
+++ src/main/java/org/springboot/mballem/MballemApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.mballem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MballemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MballemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springboot/mballem/MballemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.mballem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MballemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
