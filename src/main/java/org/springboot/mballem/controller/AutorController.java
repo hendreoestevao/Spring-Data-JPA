2f6f561eb9d2ed6f90f16c49176e39fee055fc5f
hendreo santos <hendreosantos1@outlook.com>
2026-03-13 20:41:11 -0300

    Ajustando controller etc

--- src/main/java/org/springboot/mballem/controller/AutorController.java
+++ src/main/java/org/springboot/mballem/controller/AutorController.java
@@ -2,10 +2,17 @@ package org.springboot.mballem.controller;
 
 import org.springboot.mballem.dao.AutorDAO;
 import org.springboot.mballem.entity.Autor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("autores")
@@ -22,4 +29,32 @@ public class AutorController {
         dao.save(autor);
         return autor;
     }
+
+    @PutMapping
+    public Autor atualizar(@RequestBody Autor autor) {
+        dao.update(autor);
+        return autor;
+    }
+
+    @DeleteMapping("{id}")
+    public String remover(@PathVariable Long id) {
+        dao.delete(id);
+        return "Deletado com sucesso";
+    }
+
+    @GetMapping("{id}")
+    public Autor buscarPorId(@PathVariable Long id) {
+        return dao.findById(id);
+    }
+
+    @GetMapping()
+    public List<Autor> buscarTodosAutores() {
+        return dao.findAll();
+    }
+
+    @GetMapping("nomeOrSobrenome")
+    public List<Autor> buscarPorNomeOuSobrenome(@RequestParam String args) {
+        return dao.findByNomeOrSobrenome(args);
+    }
+
 }
--- src/main/java/org/springboot/mballem/dao/AutorDAO.java
+++ src/main/java/org/springboot/mballem/dao/AutorDAO.java
@@ -6,6 +6,8 @@ import org.springboot.mballem.entity.Autor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public class AutorDAO {
 
@@ -15,5 +17,35 @@ public class AutorDAO {
     @Transactional
     public void save(Autor autor) {
         manager.persist(autor);
+    }
+
+
+    @Transactional
+    public void update(Autor autor) {
+        manager.merge(autor);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        manager.remove(manager.getReference(Autor.class, id));
+    }
+
+    @Transactional(readOnly = true)
+    public Autor findById(Long id) {
+        return manager.find(Autor.class, id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Autor> findAll() {
+        String query = "select a from Autor a";
+        return manager.createQuery(query, Autor.class).getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Autor> findByNomeOrSobrenome(String args) {
+        String query = "select a from Autor a where a.nome like :args OR a.sobrenome like :args";
+        return manager.createQuery(query, Autor.class)
+                .setParameter("args", "%" + args + "%")
+                .getResultList();
     }
 }
