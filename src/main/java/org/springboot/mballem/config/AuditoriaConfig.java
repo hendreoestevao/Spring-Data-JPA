8f6d1d01721c44229f728e28f0a56ad256261180
hendreo santos <hendreosantos1@outlook.com>
2026-03-31 20:24:29 -0300

    configurado auditoria com spring security

---
+++ src/main/java/org/springboot/mballem/config/AuditoriaConfig.java
@@ -0,0 +1,28 @@
+package org.springboot.mballem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditoriaConfig implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        User principal = (User) authentication.getPrincipal();
+        return Optional.of(principal.getUsername());
+    }
+}
